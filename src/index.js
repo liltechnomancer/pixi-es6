6fd3c29c434eba21a717e06293d2dd1bd7fcff07
Levi Robertson <robertson.l.levi@gmail.com>
2017-10-13 19:59:32 -0700

    add yarn start script

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ loader = pixi.loader,
 resources = pixi.loader.resources,
 Sprite = pixi.Sprite,
 TextureCache = pixi.utils.TextureCache
-//Create a Pixi stage and renderer and add the
-//renderer.view to the DOM
-var stage = new Container(),
+
+
+let stage = new Container(),
 renderer = autoDetectRenderer(1000, 1000);
 document.body.appendChild(renderer.view);
